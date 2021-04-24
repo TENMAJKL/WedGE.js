bf47ce7bf69a89b2cc7d73c98e435f806311cba7
TENMAJKL <61601336+TENMAJKL@users.noreply.github.com>
2021-04-24 10:39:00 +0200

    Syntax changes

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-exports.log = function (content = "I'm working!") {
+exports.log = function (content = "I'm working!") 
+{
     console.log('WedGE: ' + content)
 };
 
-exports.setup = function (htmlclass = ".gameCanvas") {
+exports.setup = function (htmlclass = ".gameCanvas") 
+{
     console.log('WedGE: Starting Setup')
     const canvas = document.querySelector('canvas')
     console.log('WedGE: Found a canvas - ' + canvas)
-    c = canvas.getContext
+    var context = canvas.getContext
 };
